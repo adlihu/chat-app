5d085b59e893e1dbb2f87d9025598bef20acb6c0
Adli Husain <adlihusain716@gmail.com>
2021-02-25 09:48:39 +0800

    Updates

---
+++ precache-manifest.3890b3bcd910035473234d16b4eacfa2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf593652d018d5bff00b8039faa2ab41",
+    "url": "/chat-app/index.html"
+  },
+  {
+    "revision": "7aef1c805525f7d395b5",
+    "url": "/chat-app/static/css/main.9328c9c0.chunk.css"
+  },
+  {
+    "revision": "0ee6761e79593f2a7912",
+    "url": "/chat-app/static/js/2.666e9855.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/chat-app/static/js/2.666e9855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aef1c805525f7d395b5",
+    "url": "/chat-app/static/js/main.2a329b10.chunk.js"
+  },
+  {
+    "revision": "844d941a2013a08fe2e0",
+    "url": "/chat-app/static/js/runtime-main.bdb3b2b2.js"
+  }
+]);
